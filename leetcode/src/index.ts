cef48be809b6866fff325c95cd5cc57c4a5d31e4
Katre Metsvahi <katre@codeyra.com>
2025-01-17 18:24:49 +0200

    feat: argument length and to be or not to be

--- leetcode/src/index.ts
+++ leetcode/src/index.ts
@@ -1,6 +1,8 @@
 import { isPalindrome } from "./isPalindrome"
 import { romanToInt } from "./roman-to-int"
 import { mergeTwoLists } from "./sum-of-two"
+import { argumentsLength } from "./arguments-length"
+import { expect } from './to-be-or-not-to-be';
 
 const roman = romanToInt("XII")
 console.log(roman)
@@ -25,3 +27,7 @@ const item4 = new ListNode(1, item5)
 const nodes = mergeTwoLists(item1, item4)
 
 console.log(isPalindrome(333))
+
+console.log("This is argumentsLength", argumentsLength({}, null, "3"))
+
+console.log("This is to be or not to be", expect(5).notToBe(5))
